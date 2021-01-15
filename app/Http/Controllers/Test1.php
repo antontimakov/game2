617482c5c21f071d634f6371b646f365db8076fa
timakov <atimakov@srv.rbt>
2021-01-15 10:18:18 +0300

    соединение с базой

--- app/Http/Controllers/Test1.php
+++ app/Http/Controllers/Test1.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Test1 extends BaseController
 {
     function index()
     {
-        echo 'hello lara';
-        event(new MyEvent(mt_rand(0, 100)));
+        $dmg = mt_rand(0, 100);
+        $res = DB::table('fire')->first();
+
+        DB::table('fire')
+            -> update(
+                [
+                    'number_fires' => $res->number_fires + 1,
+                    'damage' => $res->damage + $dmg
+                ]
+            );
+        event(new MyEvent($res->damage + $dmg . ' (' . $dmg . ')'));
+        //return 1;
     }
 }
