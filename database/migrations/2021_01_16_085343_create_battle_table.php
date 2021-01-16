447c5b8d75b428f16f65867fccce86a747f8d843
timakov <atimakov@srv.rbt>
2021-01-16 12:38:27 +0300

    миграция для создания таблицы битв

---
+++ database/migrations/2021_01_16_085343_create_battle_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBattleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('main.battle', function (Blueprint $table) {
+            $table->id();
+            $table->integer('player_id')->comment('Игрок');
+            $table->integer('hit_points')->default(0)->comment('Кол-во очков здоровья');
+            $table->integer('mana_points')->default(0)->comment('Кол-во очков маны');
+            $table->timestamps();
+
+            $table->foreign('player_id')->references('id')
+                ->on('main.player')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('battle');
+    }
+}
